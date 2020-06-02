23529f1546f08263bb47072828d828270b8edee0
victorsouza2382 <victorsouza2382@gmail.com>
2020-06-02 19:39:35 -0300

    Aula2 commit2

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,9 +7,40 @@ routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*');
 
   const serializadeItems = items.map(item => {
-    return 1;
+    return {
+      id: item.id,
+      title: item.name,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
   });
 
+  routes.post('/points', async (request, response) => {
+    const {
+      name,
+      email,
+      whatsapp,
+      latitude,
+      logitude,
+      city,
+      uf,
+      items
+    } = request.body;
+  });
+  await knex('points').insert({
+    image: 'image-fake',
+    name: "name",
+    email: "email",
+    whatsapp: "whatsapp",
+    latitude: "latitude",
+    longitude: "longitude",
+    city: "city",
+    uf: "uf"
+  });
+
+  return response.json({ sucess: true });
+
   return response.json(serializadeItems);
-})
+});
+
+
 export default routes;
